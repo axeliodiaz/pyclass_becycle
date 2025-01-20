963f858b77c07044d3fe5d005c8c99d361994941
Axel Díaz <diaz.axelio@gmail.com>
2025-01-20 13:20:08 -0300

    chore(settings): adding basic config variables

---
+++ settings.py
@@ -0,0 +1,11 @@
+SCHEDULE_ID_START = 12700
+SCHEDULE_URLS = ["https://becycle.cl/clase/99999999/", "https://becycle.cl/clase/12860/"]
+SCHEDULE_URL = "https://becycle.cl/clase/{class_id}/"
+
+SCHEDULES_WANTED = [
+    {"day": "Lunes", "time": "07:15", "instructor": "Goyo"},
+    {"day": "Miercoles", "time": "07:15", "instructor": "Victor"},
+    {"day": "Viernes", "time": "07:15", "instructor": "Ever"},
+    {"day": "Sabado", "time": "09:15", "instructor": "Goyo"},
+]
+NOT_ALLOWED_CLASS_TYPE = ["beat"]
